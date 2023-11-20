9825e95083455123edd31e48f697a940f0bdb3f3
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-11-20 12:37:09 -0700

    adds routing for individual pages

--- index.jsx
+++ index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from './pages/VanDetail'
 
 import "./server"
 
@@ -22,11 +24,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
